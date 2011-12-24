a3791c9bb2c221ea5c6e77584136989321827d34
saumya <2saumyaray@gmail.com>
2011-12-24 15:01:31 +0530

    added jQuery to track the dom event load

--- js/myMoney.js
+++ js/myMoney.js
@@ -1,18 +1,22 @@
-var screen, stack, card;
+$(document).ready(function(){
+	var screen, stack, card;
 
-// initialize jo
-jo.load();
-
-// create our UI
-screen = new joScreen(
-    stack = new joStack().push(
-        card = new joCard([
-            new joTitle("My Money."),
-            new joCaption("This is awesome."),
-            new joDivider(),
-            new joButton("OK").selectEvent.subscribe(function() {
-                stack.hide();
-            })
-        ])
-    )
-);
+	// initialize jo
+	jo.load();
+	
+	// create our UI
+	screen = new joScreen(
+	    stack = new joStack().push(
+	        card = new joCard([
+	            new joTitle("Hello"),
+	            new joCaption("Hello World!"),
+	            new joDivider(),
+	            new joButton("OK").selectEvent.subscribe(function() {
+	                stack.hide();
+	            })
+	        ])
+	    )
+	);
+	
+	//End jQuery
+});
