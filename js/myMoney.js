ae8e8dea0d95e4562843a07ac8fe15a52acd171c
saumya <2saumyaray@gmail.com>
2011-12-24 15:59:12 +0530

    wip

--- js/myMoney.js
+++ js/myMoney.js
@@ -3,17 +3,20 @@ $(document).ready(function(){
 
 	// initialize jo
 	jo.load();
-	
 	// create our UI
 	screen = new joScreen(
 	    stack = new joStack().push(
 	        card = new joCard([
 	            new joTitle("My Money"),
-	            new joCaption("Hello World!"),
+	            new joCaption('platform = '+jo.getPlatform()),
+	            new joCaption('language = '+jo.getLanguage()),
+	            new joCaption('jo version = '+jo.getVersion()),
 	            new joDivider(),
 	            new joButton("OK").selectEvent.subscribe(function() {
 	                stack.hide();
-	            })
+	            }),
+	            new joDivider(),
+	            new joMenu(['My Money','Add','update','info','test','test','test','test','test','test','last'])
 	        ])
 	    )
 	);
