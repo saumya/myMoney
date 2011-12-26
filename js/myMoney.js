0e1f204053d9dabd47caafb0d17aae2dd0551380
saumya <2saumyaray@gmail.com>
2011-12-26 12:58:55 +0530

    scroll view is activated

--- js/myMoney.js
+++ js/myMoney.js
@@ -1,25 +1,59 @@
 $(document).ready(function(){
-	var screen, stack, card;
-
-	// initialize jo
-	jo.load();
-	// create our UI
-	screen = new joScreen(
-	    stack = new joStack().push(
-	        card = new joCard([
-	            new joTitle("My Money"),
-	            new joCaption('platform = '+jo.getPlatform()),
-	            new joCaption('language = '+jo.getLanguage()),
-	            new joCaption('jo version = '+jo.getVersion()),
-	            new joDivider(),
-	            new joButton("OK").selectEvent.subscribe(function() {
-	                stack.hide();
-	            }),
-	            new joDivider(),
-	            new joMenu(['My Money','Add','update','info','test','test','test','test','test','test','last'])
-	        ])
-	    )
-	);
-	
-	//End jQuery
+	App.load();
 });
+
+App = {
+	load: function() {
+		jo.load();
+		
+		document.body.addEventListener('touchmove', function(e) {
+		    e.preventDefault();
+			joEvent.stop(e);
+		}, false);
+		
+		// this is a more complex UI with a nav bar and a toolbar
+		this.scn = new joScreen(
+			new joContainer([
+				new joFlexcol([
+					this.nav = new joNavbar(),
+					this.stack = new joStackScroller()
+				]),
+				this.toolbar = new joToolbar("This is a footer, neat huh?")
+			]).setStyle({position: "absolute", top: "0", left: "0", bottom: "0", right: "0"})
+		);
+		
+		this.nav.setStack(this.stack);
+		
+		this.stack.push(joCache.get("menu"));
+	}
+};
+
+
+
+
+joCache.set("menu", function() {
+	// some inline data and chaining going on here,
+	// dont be afraid, it'll all make sense later
+	var card = new joCard([
+		new joTitle("Select an option below"),
+		new joMenu([
+			{ title: "About", id: "about" },
+			{ title: "Form Widgets", id: "widgets" },
+			{ title: "List Views", id: "lists" },
+			{ title: "Table View", id: "tables" },
+			{ title: "Popup Dialogs", id: "popups" },
+			{ title: "Ajax Calls", id: "ajax" },
+			{ title: "Themes and CSS", id: "themes" }
+		]).selectEvent.subscribe(function(id) {
+			App.stack.push(joCache.get(id));
+		})
+	]);
+	
+	// hey, you don't have to make messy chained and
+	// inlined code; that's a coding style decision
+	// Jo doesn't pretend it should make for you.
+	card.setTitle("Jo Kitchen Sink Demo");
+	
+	return card;
+});
+
