27850df198254b88d0ba243298df25252fd40dcf
saumya <2saumyaray@gmail.com>
2012-01-07 15:17:13 +0530

    having fun learning the LungoJS

--- js/LungoJS/app/app.js
+++ js/LungoJS/app/app.js
@@ -3,9 +3,13 @@ var App = (function(lng, undefined) {
     //Define your LungoJS Application Instance
     
     lng.App.init({
-        name: 'Select a name for your app',
-        version: '0.1'
+    	id: 'saumyaID3Money',
+        name: 'Lungo Money',
+        version: '0.1',
+        platform: 'iOS'
     });
+    
+    //lng.app.get('name');
     
     return {
 
