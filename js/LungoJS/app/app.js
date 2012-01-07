c00c194fe269bfbf882a15f0cd9f8c8fe950d759
saumya <2saumyaray@gmail.com>
2012-01-07 13:38:59 +0530

    added libs and frameworks for work with SQLite

---
+++ js/LungoJS/app/app.js
@@ -0,0 +1,14 @@
+var App = (function(lng, undefined) {
+
+    //Define your LungoJS Application Instance
+    
+    lng.App.init({
+        name: 'Select a name for your app',
+        version: '0.1'
+    });
+    
+    return {
+
+    };
+
+})(LUNGO);
